2532513fe52f33a404a319ddd2e11655d1c231a7
JulianaForbici <julianaforbicii@gmail.com>
2026-07-02 21:53:29 -0300

    fix: add model de venda

--- App/Model/Venda.php
+++ App/Model/Venda.php
@@ -1,6 +1,102 @@
 <?php
 
 namespace Model;
+use Livro\Database\Transaction;
 use Livro\Database\Record;
+use Livro\Database\Repository;
+use Livro\Database\Criteria;
+class Venda extends Record
+{
+    const TABLENAME = 'venda';
+    private $itens;
+    private $cliente;
 
-class Venda {}
+    public function setCliente(Pessoa $cliente)
+    {
+        $this->cliente = $cliente;
+        $this->id_cliente = $cliente->id;
+    }
+
+    public function getCliente()
+    {
+        if (empty($this->cliente)) {
+            $this->cliente = new Pessoa($this->id_cliente);
+        }
+
+        return $this->cliente;
+    }
+
+    public function addItem(Produto $p, $quantidade)
+    {
+        $item = new ItemVenda();
+        $item->produto = $p;
+        $item->preco = $p->preco_venda;
+        $item->quantidade = $quantidade;
+        $this->itens[] = $item;
+        $this->valor_venda += $item->preco * $quantidade;
+    }
+
+    public function store()
+    {
+        parent::store();
+        foreach ($this->itens as $item) {
+            $item->id_venda = $this->id;
+            $item->store();
+        }
+    }
+
+    public function getItens()
+    {
+        $repositorio = new Repository('ItemVenda');
+        $criterio = new Criteria();
+        $criterio->add('id_venda', '=', $this->id);
+        $this->itens = $repositorio->load($criterio);
+        return $this->itens;
+    }
+
+    public static function getVendasMes()
+    {
+        $meses = [];
+        $meses[1] = 'Janeiro';
+        $meses[2] = 'Fevereiro';
+        $meses[3] = 'Março';
+        $meses[4] = 'Abril';
+        $meses[5] = 'Maio';
+        $meses[6] = 'Junho';
+        $meses[7] = 'Julho';
+        $meses[8] = 'Agosto';
+        $meses[9] = 'Setembro';
+        $meses[10] = 'Outubro';
+        $meses[11] = 'Novembro';
+        $meses[12] = 'Dezembro';
+
+        $conn = Transaction::get();
+        $result = $conn->query(
+            "select strftime('%m', data_venda) as mes, sum(valor_final) as valor from venda group by 1");
+
+        $dataset = [];
+        foreach ($result as $row) {
+            $mes = $meses[(int) $row['mes']];
+            $dataset[$mes] = $row['valor'];
+        }
+
+        return $dataset;
+    }
+
+    public static function getVendasTipo()
+    {
+        $conn = Transaction::get();
+        $result = $conn->query("  SELECT tipo.nome as tipo, sum(item_venda.quantidade*item_venda.preco) as total
+                                    FROM venda, item_venda, produto, tipo
+                                   WHERE venda.id = item_venda.id_venda 
+                                     AND item_venda.id_produto = produto.id
+                                     AND produto.id_tipo = tipo.id
+                                GROUP BY 1");
+
+        $dataset = [];
+        foreach ($result as $row) {
+            $dataset[$row['tipo']] = $row['total'];
+        }
+        return $dataset;
+    }
+}
