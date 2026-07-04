2171c50d6c4cb59e7224b003ebd0efb59953dddf
JulianaForbici <julianaforbicii@gmail.com>
2026-07-03 23:53:43 -0300

    fix: add page controller

--- Lib/Livro/Control/Page.php
+++ Lib/Livro/Control/Page.php
@@ -1,3 +1,27 @@
 <?php
 
-class Page {}
+use Livro\Widgets\Base\Element;
+
+class Page extends Element
+{
+    public function __construct()
+    {
+        parent::__construct('div');
+    }
+
+    public function show()
+    {
+        if ($_GET) {
+            $class = isset($_GET['class']) ? $_GET['class'] : '';
+            $method = isset($_GET['method']) ? $_GET['method'] : '';
+
+            if ($class) {
+                $object = $class == get_class($this) ? $this : new $class();
+                if (method_exists($object, $method)) {
+                    call_user_func([$object, $method], $_GET);
+                }
+            }
+        }
+        parent::show();
+    }
+}
