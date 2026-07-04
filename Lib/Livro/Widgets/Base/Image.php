40b1245cc2d9e014d3fb9bed58bd796633892f7f
JulianaForbici <julianaforbicii@gmail.com>
2026-07-04 00:25:23 -0300

    fix: add classe que representa uma imagem

--- Lib/Livro/Widgets/Base/Image.php
+++ Lib/Livro/Widgets/Base/Image.php
@@ -1,3 +1,15 @@
 <?php
 
-class Image {}
+use Livro\Widgets\Base\Element;
+
+class Image extends Element
+{
+    private $source;
+
+    public function __construct($source)
+    {
+        parent::__construct();
+        $this->src = $source;
+        $this->border = 0;
+    }
+}
