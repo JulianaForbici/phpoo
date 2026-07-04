d2f6a64589f25deaaa3bb0862184b7ea7413da88
JulianaForbici <julianaforbicii@gmail.com>
2026-07-04 00:24:45 -0300

    fix: add classes de messages e questions, pra mostrar mensagens ao user

---
+++ Lib/Livro/Widgets/Dialog/Question.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Widgets\Dialog;
+
+use Livro\Control\Action;
+use Livro\Widgets\Base\Element;
+
+class Question
+{
+    function __construct($message, Action $action_yes, Action $action_no = null)
+    {
+        $div = new Element('div');
+        $div->class = 'alert alert-warning question';
+        $url_yes = $action_yes->serialize();
+        $link_yes = new Element('a');
+        $link_yes->href = $url_yes;
+        $link_yes->class = 'btn btn-default';
+        $link_yes->style = 'float:right';
+        $link_yes->add('Sim');
+        $message .= '&nbsp;' . $link_yes;
+        if ($action_no) {
+            $url_no = $action_no->serialize();
+            $link_no = new Element('a');
+            $link_no->href = $url_no;
+            $link_no->class = 'btn btn-default';
+            $link_no->style = 'float:right';
+            $link_no->add('Não');
+
+            $message .= $link_no;
+        }
+        $div->add($message);
+        $div->show();
+    }
+}
