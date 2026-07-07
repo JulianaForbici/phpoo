c9d5326360c5521267984e6b067b732483fcf18b
JulianaForbici <julianaforbicii@gmail.com>
2026-07-06 21:42:14 -0300

    fix: add file e form

--- Lib/Livro/Widgets/Form/File.php
+++ Lib/Livro/Widgets/Form/File.php
@@ -2,7 +2,32 @@
 
 namespace Widgets\Form;
 
-class File
+use Livro\Widgets\Base\Element;
+use Livro\Widgets\Form\Field;
+use Livro\Widgets\Form\FormElementInterface;
+
+class File extends Field implements FormElementInterface
 {
 
+    public function show()
+    {
+        $tag = new Element('input');
+        $tag->class = 'field';
+        $tag->name = $this->name;
+        $tag->value = $this->value;
+        $tag->type = 'file';
+        $tag->style = "width:{$this->size}";
+        if (!parent::getEditable())
+        {
+            $tag->readonly = "1";
+        }
+        if ($this->properties)
+        {
+            foreach ($this->properties as $property => $value)
+            {
+                $tag->$property = $value;
+            }
+        }
+        $tag->show();
+    }
 }
