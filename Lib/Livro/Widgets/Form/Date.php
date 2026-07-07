35e65226b9c3a78f39a2c53e9262f8541d80c2b8
JulianaForbici <julianaforbicii@gmail.com>
2026-07-06 21:38:45 -0300

    fix: add date

---
+++ Lib/Livro/Widgets/Form/Date.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Widgets\Form;
+
+use Livro\Widgets\Base\Element;
+use Livro\Widgets\Form\Entry;
+use Livro\Widgets\Form\FormElementInterface;
+
+class Date extends Entry implements FormElementInterface
+{
+    public function show()
+    {
+        $tag = new Element('input');
+        $tag->class = 'field';
+        $tag->name = $this->name;
+        $tag->value = $this->value;
+        $tag->type = 'date';
+        $tag->style = "width:{$this->size}";
+        if (!parent::getEditable())
+        {
+            $tag->readonly = "1";
+        }
+        if ($this->properties)
+        {
+            foreach ($this->properties as $property => $value)
+            {
+                $tag->$property = $value;
+            }
+        }
+        $tag->show();
+    }
+}
