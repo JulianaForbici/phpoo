231a9fc18862f5ed6331dba6ea648762f20e15d5
JulianaForbici <julianaforbicii@gmail.com>
2026-07-06 21:49:54 -0300

    fix: add radiobutton e radiogroup

---
+++ Lib/Livro/Widgets/Form/RadioGroup.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Widgets\Form;
+
+use Livro\Widgets\Base\Element;
+use Livro\Widgets\Form\Field;
+use Livro\Widgets\Form\FormElementInterface;
+use Livro\Widgets\Form\Label;
+use Livro\Widgets\Form\RadioButton;
+
+class RadioGroup extends Field implements FormElementInterface
+{
+    private $layout = 'vertical';
+    private $items;
+    public function setLayout($dir)
+    {
+        $this->layout = $dir;
+    }
+    public function addItems($items)
+    {
+        $this->items = $items;
+    }
+    public function show()
+    {
+        if ($this->items)
+        {
+            foreach ($this->items as $index => $label)
+            {
+                $button = new RadioButton($this->name);
+                $button->setValue($index);
+                if ($this->value == $index)
+                {
+                    $button->setProperty('checked', '1');
+                }
+
+                $obj = new Label($label);
+                $obj->add($button);
+                $obj->show();
+                if ($this->layout == 'vertical')
+                {
+                    $br = new Element('br');
+                    $br->show();
+                }
+                echo "\n";
+            }
+        }
+    }
+}
