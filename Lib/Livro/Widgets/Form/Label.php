93c768b0c2aff0532765fd54dd872488cf3be97c
JulianaForbici <julianaforbicii@gmail.com>
2026-07-06 21:49:40 -0300

    fix: add label

---
+++ Lib/Livro/Widgets/Form/Label.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Widgets\Form;
+
+use Livro\Widgets\Base\Element;
+use Livro\Widgets\Form\Field;
+use Livro\Widgets\Form\FormElementInterface;
+
+class Label extends Field implements FormElementInterface
+{
+    private $tag;
+
+    public function __construct($value)
+    {
+        $this->setValue($value);
+        $this->tag = new Element('label');
+    }
+
+    public function add($child)
+    {
+        $this->tag->add($child);
+    }
+
+    public function show()
+    {
+        $this->tag->add($this->value);
+        $this->tag->show();
+    }
+}
